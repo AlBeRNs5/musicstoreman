71b8367065252c21070b3bbe866150e0092c5f86
AlBeRNs5 <46230913+AlBeRNs5@users.noreply.github.com>
2019-02-07 19:11:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const gif = require("gif-search");
 const client = new Discord.Client({disableEveryone: true});
 
 const prefix = "so";
-var adminprefix = 'so'
+var adminprefix = '*so'
 
 
 client.on('message', async msg =>{
